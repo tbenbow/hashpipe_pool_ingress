4f9d536a8f3ce3944dbde3a25a15d5beb5460425
Tom Benbow <tom@hellomoon.net>
2020-07-02 10:28:00 -0500

    add env-sample.js

---
+++ env-sample.js
@@ -0,0 +1,21 @@
+/** InfluxDB v2 URL */
+const url = process.env['INFLUXDB_URL'] || 'MY_URL'
+/** InfluxDB authorization token */
+const token = process.env['INFLUXDB_TOKEN'] || 'MY_INFLUX_TOKEN'
+/** Organization within InfluxDB URL  */
+const org = process.env['INFLUXDB_ORG'] || 'hellomoon'
+/**InfluxDB bucket used in examples  */
+const bucket = 'MY_BUCKET'
+/**InfluxDB user  */
+const username = 'my-user'
+/**InfluxDB password  */
+const password = 'my-password'
+
+module.exports = {
+  url,
+  token,
+  org,
+  bucket,
+  username,
+  password,
+}
